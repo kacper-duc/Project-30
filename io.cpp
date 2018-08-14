7da14d23e77bea0bc7300e50ef3a15ff1fbf53c6
kacper-duc <mercurry07@gmail.com>
2018-08-14 23:22:33 +0200

    123

--- io.cpp
+++ io.cpp
@@ -1,19 +1,19 @@
 #include "stdafx.h"
 #include <iostream>
-//using namespace std;
-//
-//int a, b;
-//
-//void readNumber()
-//{
-//	cout << "Please enter first number: ";
-//	cin >> a;
-//	cout << endl << "Please enter second number: ";
-//	cin >> b;
-//	cout << endl << "The sum is equal to: ";
-//}
-//
-//int writeNumber()
-//{
-//	return a + b;
-//}
+using namespace std;
+
+int a, b;
+
+void readNumber()
+{
+	cout << "Please enter first number: ";
+	cin >> a;
+	cout << endl << "Please enter second number: ";
+	cin >> b;
+	cout << endl << "The sum is equal to: ";
+}
+
+int writeNumber()
+{
+	return a + b;
+}
